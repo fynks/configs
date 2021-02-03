aa928f740d70a5a7a4948cf292e9f1803a711d1d
Fynks <75840152+fynks@users.noreply.github.com>
2021-02-03 23:02:44 +0500

    :cyclone: Major changes :
    - Added hblock install support
    - Added Thunderbird
    - Replaced stacer with bleachbit

--- scripts/apps_installer.sh
+++ scripts/apps_installer.sh
@@ -35,8 +35,11 @@ echo ""
 echo "Installing Evince..."
 sudo pacman -S evince
 echo ""
-echo "Installing Stacer ..."
-sudo pacman -S stacer
+echo "Installing Bleachbit ..."
+sudo pacman -S bleachbit
+echo ""
+echo "Installing Thunderbird ..."
+sudo pacman -S thunderbird
 echo ""
 echo "Installing Caffeine-ng..."
 sudo pacman -S caffeine-ng
@@ -77,10 +80,19 @@ echo "Installing fisher"
 ccurl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 echo""
 echo"Now you can install IlanCosman/tide by going here https://github.com/IlanCosman/tide"
+echo ""
+echo "------------------------------------------------------------"
+echo ""
+echo "Installing hblock"
 
+curl -o /tmp/hblock 'https://raw.githubusercontent.com/hectorm/hblock/v3.2.0/hblock' \
+  && echo 'b9ed6de52455fbde882879ef50470c1538bc5ac8d1479ef130442770b159dbe3  /tmp/hblock' | shasum -c \
+  && sudo mv /tmp/hblock /usr/local/bin/hblock \
+  && sudo chown 0:0 /usr/local/bin/hblock \
+  && sudo chmod 755 /usr/local/bin/hblock
 
-
-
+echo""
+echo"run hblock for updating hosts,for more visist : https://github.com/hectorm/hblock "
 
 
 
