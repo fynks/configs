6925b9869e3867680a9b2c3f0ca6b7211bfef5dc
fynks <75840152+fynks@users.noreply.github.com>
2022-09-10 21:35:03 +0500

    removed colors

--- setup/vs_code_config.sh
+++ setup/vs_code_config.sh
@@ -10,25 +10,14 @@
 #  VS Code fix for KDE Linux
 #-------------------------------------------------------------------------
 
-
-# Color Configs
-GREEN="\e[32"
-YELLOW="\e[33"
-BLUE="\e[34"
-BOLDGREEN="\e[1;${GREEN}m"
-ITALICGREEN="\e[3;${GREEN}m"
-ITALICYELLOW="\e[3;${YELLOW}m"
-ITALICBLUE="\e[3;${BLUE}m"
-ENDCOLOR="\e[0m"
-
 # Copies the custom sources file from github to /etc/hblock/sources.list
  echo -e "\n##########################################\n######### Fixing VS Code for KDE #########\n##########################################\n"
- echo -e ">> ${ITALICGREEN}Installing the required packages${ENDCOLOR} <<"
+ echo -e "Installing the required packages"
 sudo pacman -S --noconfirm gnome-keyring libsecret libgnome-keyring
 
- echo -e ">> ${ITALICGREEN}copying the files to required directory${ENDCOLOR} <<"
+ echo -e "copying the files to required directory"
  sudo  curl -o ~/.xinitrc 'https://raw.githubusercontent.com/fynks/configs/main/setup/sample_xinitrc_file'
 
-echo -e "\n >> ${ITALICBLUE}DONE ${ENDCOLOR} << \n"
-echo -e "\n >> ${ITALICYELLOW}open seahorse,unlock using password then Logout and login then login into github in VS Code ${ENDCOLOR} << \n"
+echo -e "\n DONE  \n"
+echo -e "\n open seahorse,unlock using password then Logout and login then login into github in VS Code \n"
 exit 
