6679b59ad278625e0ca276da6484a40589ee87df
fynks <75840152+fynks@users.noreply.github.com>
2022-09-10 21:40:53 +0500

    added sleep in fish_config.sh

--- setup/fish_config.sh
+++ setup/fish_config.sh
@@ -14,11 +14,6 @@
 echo -e "\n############################################\n######### Intalling fisher and tide #########\n############################################\n"
 sudo curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher && fisher install ilancosman/tide
 
-# fish abbreviation setup
-echo -e "\n############################################\n#### opening the fish abbreviatons windows ####\n############################################\n"
-echo "Close the browser window and press ENTER to continue"
-fish_config
-
 # Remove welcome to fish annoying message
 echo -e "\n############################################\n#### Removing fish welcome message ####\n############################################\n"
 touch ~/.config/fish/functions/fish_greeting.fish
@@ -37,6 +32,9 @@ echo -e "\n############################################\n######### chaning shell
 echo "please enter fish in selection"
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh
+
+# Wait for input
+sleep 2
 
 # logging out for completing the process
 echo -e "\n############################################\n######### Logging out #########\n############################################\n"
