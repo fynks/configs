d3d9690679f28117dc55311548abfa72b604f3b7
fynks <75840152+fynks@users.noreply.github.com>
2023-04-01 21:48:10 +0500

    rewrote the whole script and merged aur into setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -10,34 +10,88 @@
 #  Arch Linux Post Install Setup and Config Script
 #-------------------------------------------------------------------------
 
-# Updates the mirrors and force refreshes the repository databases
+# Check if user is root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
+# Updates the mirrors
 echo -e "\n############################################\n######### Pacman Branch,mirrors and databases updates #########\n############################################\n"
 echo "Updating mirrors with fastest mirrors"
-sudo pacman-mirrors --fasttrack 10
+if ! sudo pacman-mirrors --fasttrack 10; then
+    echo "Error updating mirrors. Aborting."
+    exit 1
+fi
 
-echo "\nSyncing databases \n"
+# Syncs the detabases
+echo -e "\nSyncing databases\n"
 sudo pacman -Syu
 
-# Start of installing packges
+# Install yay if not installed
+echo -e "\nChecking if yay is available\n"
+if ! pacman -Qs yay > /dev/null; then
+    echo "yay is not installed. Installing yay..."
+    pacman -S --needed --noconfirm base-devel yay
+    if ! pacman -Qs yay > /dev/null; then
+        echo "Error installing yay. Aborting."
+        exit 1
+    fi
+fi
+
+# Update the system before installing packages
+echo -e "\n############################################\n######### Updating system before installing packages #########\n############################################\n"
+sudo pacman -Syu --noconfirm
+
+# Start of installing packages
 echo -e "\n############################################\n######### Starting installing packages #########\n############################################\n"
 
-# List of packages to be installed from pacman
-PACMANPKGS=(
-'base-devel'              # Base Developement package
-'cmake'                   # Required for various pakage compililations
-'yay'                     # AUR Helper
+# Array of package names to install
+packages=(
+    "base-devel"
+    "cmake"
+    "alacritty"
+    "fish"
+    "plasma-wayland-session"
+    "firefox"
+    "librewolf-bin"
+    "nemo"
+    "vlc"
+    "evince"
+    "bleachbit"
+    "sublime-text-4"
+    "visual-studio-code-bin"
+    "libreoffice-fresh"
+    "nodejs"
+    "npm"
+    "android-tools"
+    "tor-browser"
+    "ventoy-bin"
+    "google-chrome"
+    "appimagelauncher"
+    "telegram-desktop"
+    "simplescreenrecorder"
+    "warpinator-git"
+    "converseen"
+    "celluloid"
+    "gimp"
 )
 
-# Installs and names the package being installed by pacman one by one
-for PACMAN in "${PACMANPKGS[@]}"; do
- echo -e "\n############################################\n######### Program : $PACMAN #########\n############################################\n"
-   sudo pacman --needed --noconfirm -S $PACMAN
+# Install packages from AUR
+for package in "${packages[@]}"; do
+    echo -e "\n############################################\n######### Installing ${package} #########\n############################################\n"
+    if ! sudo -u "$SUDO_USER" yay -S --needed --noconfirm "$package"; then
+        echo "Error installing ${package}. Aborting."
+        exit 1
+    fi
 done
 
-# Invokes the aur_packages.sh script for installing extra packages form AUR
- echo -e "\n############################################\n######### Initiating installing packages from AUR  #########\n############################################\n"
-echo "Script launch successful"
-sudo chmod +x "aur_packages.sh"
-./aur_packages.sh
+echo -e "\n############################################\n######### All packages installed #########\n############################################\n"
 
-exit 
+# Invokes the fish_config.sh script for fish shell setup
+ echo -e "\n############################################\n######### Initiating Fish config  #########\n############################################\n"
+echo "Initiation sucessfull"
+sudo chmod +x "fish_config.sh"
+./fish_config.sh
+
+exit 0
