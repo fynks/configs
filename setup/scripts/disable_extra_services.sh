4eb099e35a9734830fca778c84f847c48ac84939
fynks <you@example.com>
2023-08-31 17:21:34 +0500

    redone the script with error handling and checks

--- setup/scripts/disable_extra_services.sh
+++ setup/scripts/disable_extra_services.sh
@@ -9,20 +9,63 @@
 #                                       
 #  Arch Linux script for disabling extra services
 #-------------------------------------------------------------------------
+# Define an associative array for services and their descriptions
+declare -A services=(
+    ["bluetooth"]="Bluetooth"
+    ["lvm2-monitor"]="LVM2 Monitor"
+    ["docker"]="Docker"
+    ["ModemManager"]="ModemManager"
+)
 
-printf "\n##################################################################################################\n#### Disabling and masking bluetooth service ####\n##################################################################################################\n"
-sudo systemctl disable bluetooth.service
-sudo systemctl mask bluetooth.service
+# Function to check if a service is active
+is_service_active() {
+    sudo systemctl is-active --quiet "${1}.service"
+    return $?
+}
 
-printf "\n##################################################################################################\n#### Disabling and masking lvm2-monitor service ####\n##################################################################################################\n"
-sudo systemctl disable lvm2-monitor.service
-sudo systemctl mask lvm2-monitor.service
+# Function to check if a service is disabled and masked
+is_service_disabled_and_masked() {
+    sudo systemctl is-enabled --quiet "${1}.service" && sudo systemctl is-masked --quiet "${1}.service"
+    return $?
+}
 
-printf "\n##################################################################################################\n#### Stopping,disabling and masking docker service ####\n##################################################################################################\n"
-sudo systemctl stop docker.service
-sudo systemctl disable docker.service
-sudo systemctl mask docker.service
+# Function to disable and mask a service
+disable_and_mask_service() {
+    local service="$1"
+    local description="$2"
 
-printf "\n##################################################################################################\n#### Disabling and masking ModemManager service####\n##################################################################################################\n"
-sudo systemctl disable ModemManager.service
-sudo systemctl mask ModemManager.service
+    if is_service_disabled_and_masked "$service"; then
+        echo "$description service is already disabled and masked. Skipping..."
+        return
+    fi
+
+    if is_service_active "$service"; then
+        sudo systemctl stop "${service}.service"
+        if [ $? -eq 0 ]; then
+            echo "Stopped $description service."
+        else
+            echo "Error stopping $description service."
+        fi
+    else
+        echo "$description service is already inactive."
+    fi
+
+    sudo systemctl disable "${service}.service"
+    sudo systemctl mask "${service}.service"
+
+    if [ $? -eq 0 ]; then
+        echo "Successfully disabled and masked $description service."
+    else
+        echo "Error occurred while disabling and masking $description service."
+    fi
+}
+
+# Loop through the services and perform actions
+for service in "${!services[@]}"; do
+    description="${services[$service]}"
+    printf "\n##################################################################################################\n"
+    echo "#### Disabling and masking $description service ($service) ####"
+    printf "##################################################################################################\n"
+
+    disable_and_mask_service "$service" "$description"
+done
