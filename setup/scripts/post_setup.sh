e08183a16de439ca982a97637f0023afd68f2de6
fynks <you@example.com>
2023-09-01 12:34:09 +0500

    merged post and disable services

--- setup/scripts/post_setup.sh
+++ setup/scripts/post_setup.sh
@@ -17,54 +17,121 @@ print_section_header() {
     printf "############################################\n\n"
 }
 
-# Whiptail prompt
-function prompt() {
+# Function to display a highlighted prompt and ask for user confirmation
+prompt() {
     local message="$1"
     whiptail --title "Confirmation" --yesno "$message 
     Do you want to continue?" 10 50
     return $?
 }
 
+
 # Function to install packages if missing
 install_packages_if_needed() {
     for pkg in "$@"; do
-        if ! pacman -Q "$pkg" &> /dev/null; then
-            sudo pacman -S --needed --noconfirm "$pkg"
-        fi
+        pacman -Q "$pkg" &>/dev/null || sudo pacman -S --needed --noconfirm "$pkg"
     done
 }
 
-# Copies the librewolf.overrides.cfg from GitHub to $Home/.librewolf/
+# Function to download a file
+download_file() {
+    local url="$1"
+    local destination="$2"
+    if ! curl -o "$destination" "$url"; then
+        echo "Error: Failed to download $url"
+        exit 1
+    fi
+}
+
+# Section: Setting up Librewolf override.config
 print_section_header "Setting up Librewolf override.config"
-sudo curl -o $HOME/.librewolf/librewolf.overrides.cfg 'https://raw.githubusercontent.com/fynks/configs/main/setup/configs/librewolf.overrides.cfg'
+mkdir -p "$HOME/.librewolf/"
+download_file 'https://raw.githubusercontent.com/fynks/configs/main/setup/configs/librewolf.overrides.cfg' "$HOME/.librewolf/librewolf.overrides.cfg"
 
-
-# Copies the custom sources file from GitHub to /etc/hblock/sources.list
-print_section_header "configuring hblock"
-sudo mkdir -p /etc/hblock/ &&
-sudo curl -o /etc/hblock/sources.list 'https://raw.githubusercontent.com/fynks/configs/main/setup/configs/hblock_sources.list'
+# Section: Configuring hblock
+print_section_header "Configuring hblock"
+sudo mkdir -p /etc/hblock/
+download_file 'https://raw.githubusercontent.com/fynks/configs/main/setup/configs/hblock_sources.list' '/etc/hblock/sources.list'
 sudo hblock
 
-# Enables docker service
+# Section: Fixing VS Code for KDE
+print_section_header "Fixing VS Code for KDE"
+install_packages_if_needed gnome-keyring libsecret libgnome-keyring
+echo -e "Copying the files to the required directory"
+download_file 'https://raw.githubusercontent.com/fynks/configs/main/setup/configs/sample_xinitrc_file' "$HOME/.xinitrc"
+echo -e "\n DONE  \n"
+echo -e "\n Open Seahorse, unlock using your password, then log out and log in again. After that, log in to GitHub in VS Code.\n"
+
+# Prompt for disabling services
+prompt "Now script will update mirrors and start installing package" || exit 0
+
+declare -A services=(
+    ["bluetooth"]="Bluetooth"
+    ["lvm2-monitor"]="LVM2 Monitor"
+    ["docker"]="Docker"
+    ["ModemManager"]="ModemManager"
+)
+
+# Function to check if a service is active
+is_service_active() {
+    sudo systemctl is-active --quiet "${1}.service"
+    return $?
+}
+
+# Function to check if a service is disabled and masked
+is_service_disabled_and_masked() {
+    sudo systemctl is-enabled --quiet "${1}.service" && sudo systemctl is-masked --quiet "${1}.service"
+    return $?
+}
+
+# Function to disable and mask a service
+disable_and_mask_service() {
+    local service="$1"
+    local description="$2"
+
+    if is_service_disabled_and_masked "$service"; then
+        echo "$description service is already disabled and masked. Skipping..."
+        return
+    fi
+
+    if is_service_active "$service"; then
+        sudo systemctl stop "${service}.service"
+        if [ $? -eq 0 ]; then
+            echo "Stopped $description service."
+        else
+            echo "Error stopping $description service."
+        fi
+    else
+        echo "$description service is already inactive."
+    fi
+
+    sudo systemctl disable "${service}.service"
+    sudo systemctl mask "${service}.service"
+
+    if [ $? -eq 0 ]; then
+        echo "Successfully disabled and masked $description service."
+    else
+        echo "Error occurred while disabling and masking $description service."
+    fi
+}
+
+# Loop through the services and perform actions
+for service in "${!services[@]}"; do
+    description="${services[$service]}"
+    print_section_header "Disabling and masking $description service ($service)"
+    disable_and_mask_service "$service" "$description"
+done
+
+# Section: Setup Complete, please reboot
+print_section_header "Setup Complete, please reboot"
+
+# Optional: Enabling docker-service
+prompt "Do you want to enable Docker services" || exit 0
 print_section_header "Enabling docker-service"
 sudo systemctl start docker.service
 sudo systemctl enable docker.service
 
-# Fixing VS Code for KDE
-print_section_header "Fixing VS Code for KDE"
-install_packages_if_needed gnome-keyring libsecret libgnome-keyring
-echo -e "Copying the files to the required directory"
-curl -o ~/.xinitrc 'https://raw.githubusercontent.com/fynks/configs/main/setup/configs/sample_xinitrc_file'
-echo -e "\n DONE  \n"
-echo -e "\n Open Seahorse, unlock using your password, then log out and log in again. After that, log in to GitHub in VS Code.\n"
-
-# Reeboot info
-print_section_header "Setup Complete, please reboot"
+print_section_header "Everything is Done,Please Reboot"
 sleep 1
 
-# Prompts for disabling services script
-prompt "Do you want to disable extra services?" || exit 0
-sudo chmod +x "./disable_extra_services.sh"
-sudo "./disable_extra_services.sh"
-
-exit
+exit 0
