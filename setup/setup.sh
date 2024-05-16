4d34bea461f4495a4b9eeac0c0258c57768fb64e
fynks <75840152+fynks@users.noreply.github.com>
2024-05-16 18:17:21 +0500

    chore: Update Firefox policies file path in setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -140,7 +140,7 @@ print_section_header "Necessary packages installation successful"
 
 # copy firefox policies
 print_section_header "Copying Firefox policies"
-if ! sudo mkdir /etc/firefox/policies/ && sudo cp ~/configs/browsers/firefox_policies.json /etc/firefox/policies/policies.json; then
+if ! sudo mkdir /etc/firefox/policies/ && sudo cp ~/configs/browsers/policies.json /etc/firefox/policies/policies.json; then
     handle_error "Error copying firefox policies"
 fi
 
