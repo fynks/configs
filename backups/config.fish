20c60b73aa5ecebe0b958ea22337a7327f285d80
fynks <75840152+fynks@users.noreply.github.com>
2025-11-08 18:58:35 +0500

    fix: correct alias for chmod and update ls command for consistency

--- backups/config.fish
+++ backups/config.fish
@@ -1,18 +1,30 @@
-### EXPORT ###
-set fish_greeting  
+set fish_greeting ''
+set -gx EDITOR subl
 
 ### ALIASES ###
 # Navigation
 alias ..='cd ..'
 alias ...='cd ../..'
 alias cl='clear'
-alias chx= 'chmod +x'
+
+# Safer `chmod +x` (avoids accidental overwrite)
+alias chx='chmod u+x'
+
+# Fish
 alias src='source ~/.config/fish/config.fish'
+
+# Pacman (Arch)
 alias update-mirrors='sudo pacman-mirrors --fasttrack 5 && sudo pacman -Syu'
 
 # Changing "ls" to "eza"
-alias ls='eza -a --color=always --icons --group-directories-first' # my preferred listing
-alias ll='eza -al --color=always --group-directories-first' # detailed listing
+alias ls='eza -al --color=always --icons --group-directories-first' # my preferred listing
+alias ll='eza -al --color=always --icons --group-directories-first --header --git' # detailed listing
+
+
+# Bat as cat replacement
+if command -v bat &>/dev/null
+    alias cat='bat --paging=never --style=plain'
+end
 
 ### SETUP ZOXIDE ###
 zoxide init fish | source
